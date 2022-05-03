c1592f431796227d1ac478842320cc75a842818f
Russell Gordon <github@russellgordon.ca>
2022-05-03 06:37:17 -0400

    Now tracking state of all nine tiles on game board.

--- NoughtsAndCrosses/NoughtsAndCrosses/Views/Helpers/TileView.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/Views/Helpers/TileView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct TileView: View {
     
     // MARK: Stored properties
-    let state: String
+    
+    // Receives the connection to a source of truth
+    // in the calling view.
+    // When state changes here, it's new value is
+    // sent there to be saved.
+    @Binding var state: String
     
     // MARK: Computed property
     var body: some View {
@@ -18,14 +23,23 @@ struct TileView: View {
             .font(.largeTitle)
             .frame(width: 50, height: 50)
             .border(Color.black, width: 3)
+            // Ensure the "hit box" is the entire view
+            // (Normally transparent parts of view cannot be tapped)
+            .contentShape(Rectangle())
+            // Taps on view uses the tile
+            .onTapGesture {
+                
+                // Temporarily make this a nought
+                state = nought
+            }
     }
     
 }
 
 struct TileView_Previews: PreviewProvider {
     static var previews: some View {
-        TileView(state: cross)
-        TileView(state: nought)
-        TileView(state: empty)
+        TileView(state: .constant(empty))
+        TileView(state: .constant(nought))
+        TileView(state: .constant(cross))
     }
 }
