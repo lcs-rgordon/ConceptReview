281f9aa6401b91b002ee8d8a58e305601e8feca6
Russell Gordon <github@russellgordon.ca>
2022-05-03 06:30:29 -0400

    Game board laid out with a static interface.

--- NoughtsAndCrosses/NoughtsAndCrosses/Views/GameBoardView.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/Views/GameBoardView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 /// - Tag: game_board
 struct GameBoardView: View {
+    
+    // MARK: Stored properties
+    
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("This is the game board.")
+        
+        VStack {
+            
+            // Top row
+            HStack {
+                TileView(state: nought)
+                TileView(state: cross)
+                TileView(state: nought)
+            }
+
+            // Middle row
+            HStack {
+                TileView(state: empty)
+                TileView(state: cross)
+                TileView(state: cross)
+            }
+
+            // Bottom row
+            HStack {
+                TileView(state: empty)
+                TileView(state: empty)
+                TileView(state: nought)
+            }
+            
+        }
+        
     }
 }
 
