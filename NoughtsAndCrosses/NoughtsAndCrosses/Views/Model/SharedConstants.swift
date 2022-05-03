66fc4fb78817a75e7a69646312442ae9d91c7a16
Russell Gordon <github@russellgordon.ca>
2022-05-03 06:25:57 -0400

    Constants to model state of a tile on the game board.

---
+++ NoughtsAndCrosses/NoughtsAndCrosses/Views/Model/SharedConstants.swift
@@ -0,0 +1,13 @@
+//
+//  SharedConstants.swift
+//  NoughtsAndCrosses
+//
+//  Created by Russell Gordon on 2022-05-03.
+//
+
+import Foundation
+
+// Constants to describe the three possible states for a tile on the game board
+let empty = ""
+let nought = "⭘"
+let cross = "✕"
