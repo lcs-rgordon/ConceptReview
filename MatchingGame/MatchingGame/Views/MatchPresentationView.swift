c27da710e71df1bd56a62bbdec6868f1120f0662
Russell Gordon <github@russellgordon.ca>
2022-05-04 10:33:54 -0400

    Matching Game: Answer now is checked for accuracy.

--- MatchingGame/MatchingGame/Views/MatchPresentationView.swift
+++ MatchingGame/MatchingGame/Views/MatchPresentationView.swift
@@ -23,39 +23,120 @@ struct MatchPresentationView: View {
     
     // The correct item
     @State var correctItem: ItemToMatch = testItem
-
+    
+    // Has a response been given and an answer checked?
+    @State var answerChecked = false
+    
+    // Was the answer correct?
+    @State var answerCorrect = false
+    
     // MARK: Computed properties
     var body: some View {
         VStack {
-
+            
+            Spacer()
+            
+            // Options
             HStack(spacing: 5) {
+                
                 Image(upperLeft.image)
                     .resizable()
                     .scaledToFit()
                     .border(.black, width: 3)
+                    .onTapGesture {
+                        
+                        // Is this the correct image?
+                        if upperLeft.description == correctItem.description {
+                            answerCorrect = true
+                        }
+                        
+                        // Answer has been checked
+                        answerChecked = true
+                    }
+                
                 Image(upperRight.image)
                     .resizable()
                     .scaledToFit()
                     .border(.black, width: 3)
+                    .onTapGesture {
+                        
+                        // Is this the correct image?
+                        if upperRight.description == correctItem.description {
+                            answerCorrect = true
+                        }
+                        
+                        // Answer has been checked
+                        answerChecked = true
+                    }
+                
             }
-
+            
             HStack(spacing: 5) {
+                
                 Image(lowerLeft.image)
                     .resizable()
                     .scaledToFit()
                     .border(.black, width: 3)
+                    .onTapGesture {
+                        
+                        // Is this the correct image?
+                        if lowerLeft.description == correctItem.description {
+                            answerCorrect = true
+                        }
+                        
+                        // Answer has been checked
+                        answerChecked = true
+                    }
+                
                 Image(lowerRight.image)
                     .resizable()
                     .scaledToFit()
                     .border(.black, width: 3)
+                    .onTapGesture {
+                        
+                        // Is this the correct image?
+                        if lowerRight.description == correctItem.description {
+                            answerCorrect = true
+                        }
+                        
+                        // Answer has been checked
+                        answerChecked = true
+                    }
+                
             }
             
+            // Prompt
             HStack {
                 Text("Which one is the \n**\(correctItem.description)?**")
                     .multilineTextAlignment(.center)
             }
             .padding(.vertical)
-
+            
+            // Result
+            ZStack {
+                Image(systemName: "checkmark.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.green)
+                // Only show this when the answer given is correct
+                //            CONDITION          true  false
+                    .opacity(answerCorrect == true ? 1.0 : 0.0)
+                
+                Image(systemName: "x.square")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.red)
+                // Show this when both of the following situations are true:
+                // 1. Answer has been checked.
+                // 2. Answer was not correct.
+                // Necessary since if we show this only when an answer is incorrect,
+                // with no other conditions, it would show as soon as a new
+                // question is generated.
+                //                 CONDITION1  AND  CONDITION2             true  false
+                    .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+            }
+            .padding(.horizontal, 75)
+            
         }
         .padding(.horizontal)
         // Hide items at first
@@ -86,7 +167,7 @@ struct MatchPresentationView: View {
             
             // Show the items
             itemsHidden = false
-
+            
         }
     }
 }
