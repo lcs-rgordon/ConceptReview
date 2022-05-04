ebb492c0b5e509e44623a7d16c5581c2dbd56136
Russell Gordon <github@russellgordon.ca>
2022-05-04 09:26:50 -0400

    Data is now loaded from the JSON file into app memory.

--- MatchingGame/MatchingGame/Views/MatchingView.swift
+++ MatchingGame/MatchingGame/Views/MatchingView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct MatchingView: View {
+    
+    // MARK: Stored properties
+    
+    // Stores the entire list of items we could make matches from
+    @State var possibleItems: [ItemToMatch] = []
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("This is where the matching interface will show.")
+        VStack {
+            Text("This is where the matching interface will show.")
+            
+        }
+        .task {
+            // Set list of flavours to the default list from the app bundle
+            let url = Bundle.main.url(forResource: "items-to-match-with", withExtension: "json")!
+            
+            // Load the contents of the JSON file
+            let data = try! Data(contentsOf: url)
+            
+            // DEBUG: What data was pulled from that file?
+            print("Data loaded from JSON file in app bundle had this data...")
+            print("===")
+            print(String(data: data, encoding: .utf8)!)
+
+            // Convert each JSON object into instances of the structure in the list
+            possibleItems = try! JSONDecoder().decode([ItemToMatch].self, from: data)
+            
+            // DEBUG: How many items are there in the list now?
+            print(dump(possibleItems))
+        }
+        
     }
+    
 }
 
 struct MatchingView_Previews: PreviewProvider {
