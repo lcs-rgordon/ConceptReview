141abfb3464b41f79d60c1ebd8e91a6488f48ffd
Russell Gordon <github@russellgordon.ca>
2022-05-04 08:54:15 -0400

    Initial project organization for Matching Game.

---
+++ MatchingGame/MatchingGame/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  MatchingGame
+//
+//  Created by Russell Gordon on 2022-05-04.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MatchingView()
+        }
+    }
+}
---
+++ MatchingGame/MatchingGame/Views/MatchingView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchingView.swift
+//  MatchingGame
+//
+//  Created by Russell Gordon on 2022-05-04.
+//
+
+import SwiftUI
+
+struct MatchingView: View {
+    var body: some View {
+        Text("This is where the matching interface will show.")
+    }
+}
+
+struct MatchingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchingView()
+    }
+}
